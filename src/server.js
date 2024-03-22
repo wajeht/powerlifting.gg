96e1614199551784922c3ff99a77e1b6da70a062
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-22 14:03:35 -0500

    feat: Add graceful shutdown functionality

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 import { app } from './app.js';
 import { env } from './conifg/env.js';
-import { logger } from './utils/logger.js'
+import { logger } from './utils/logger.js';
 
-app.listen(env.port, () => {
+const server = app.listen(env.port, () => {
 	logger.info(`Server was started on http://localhost:${env.port}`);
+});
+
+function gracefulShutdown() {
+	logger.info('Received kill signal, shutting down gracefully.');
+	server.close(() => {
+		logger.info('HTTP server closed.');
+		process.exit(0);
+	});
+}
+
+process.on('SIGINT', gracefulShutdown);
+
+process.on('SIGTERM', gracefulShutdown);
+
+process.on('unhandledRejection', (reason, promise) => {
+	logger.error('Unhandled Rejection at: ', promise, ' reason: ', reason);
 });
