1ba4164ebaea7999a49811285cfc63ed2785ef8a
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-22 15:58:06 -0500

    style: Update logger format with spread operator

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,15 +2,15 @@ export const logger = {
 	debug: (...value) => {
 		if (process.env.DEBUG === 'true' || process.env.NODE_ENV !== 'production') {
 			const timestamp = new Date().toLocaleString();
-			console.log(`\x1b[33m 🐛 ${timestamp} ${value}\x1b[0m`);
+			console.log(`\x1b[33m 🐛 ${timestamp}`, ...value, '\x1b[0m');
 		}
 	},
 	error: (...value) => {
 		const timestamp = new Date().toLocaleString();
-		console.log(`\x1b[31m ❌ ${timestamp} ${value}\x1b[0m`);
+		console.error(`\x1b[31m ❌ ${timestamp}`, ...value, '\x1b[0m');
 	},
 	info: (...value) => {
 		const timestamp = new Date().toLocaleString();
-		console.log(`\x1b[32m ✅ ${timestamp} ${value}️\x1b[0m`);
+		console.info(`\x1b[32m ✅ ${timestamp}`, ...value, '\x1b[0m');
 	},
 };
