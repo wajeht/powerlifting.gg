9b3416760d5cd281c53330fa6578b170103f5db2
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-22 21:34:31 -0500

    feat: Add login and register routes and views

--- src/views/routes.js
+++ src/views/routes.js
@@ -9,6 +9,38 @@ routes.get('/healthz', (req, res) => {
 	return res.status(200).json({ message: 'ok', date: new Date() });
 });
 
+routes.get('/login', tenantHandler, async (req, res, next) => {
+	try {
+		return res.status(200).render('login.html');
+	} catch (error) {
+		next(error);
+	}
+});
+
+routes.post('/login', tenantHandler, async (req, res, next) => {
+	try {
+		// login
+	} catch (error) {
+		next(error);
+	}
+});
+
+routes.get('/regiser', tenantHandler, async (req, res, next) => {
+	try {
+		return res.status(200).render('register.html');
+	} catch (error) {
+		next(error);
+	}
+});
+
+routes.post('/regiser', tenantHandler, async (req, res, next) => {
+	try {
+		// regiser
+	} catch (error) {
+		next(error);
+	}
+});
+
 routes.get('/', tenantHandler, async (req, res, next) => {
 	try {
 		const domain = env.env === 'production' ? 'subdomain.jaw.dev' : `localhost:${env.port}`;
@@ -17,7 +49,7 @@ routes.get('/', tenantHandler, async (req, res, next) => {
 			...tenant,
 			domain: `http://${tenant.slug}.${domain}`,
 		}));
-		return res.status(200).render('./home.html', { tenants });
+		return res.status(200).render('home.html', { tenants });
 	} catch (error) {
 		next(error);
 	}
