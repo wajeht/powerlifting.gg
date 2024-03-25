f3edfe7400bf888ea4e10e64ca06ed709b3fadd9
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-24 19:25:30 -0500

    feat: add hello-world component with count variable

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,37 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 const parseComponentArg = () => {
-  const arg =
-    process.env.COMPONENT_ARG ||
-    "src/views/components/head-world.vue:MyComponent";
-  const [path, name] = arg.split(":");
-  return { path, name };
+	const arg = 'src/views/components/hello-world.vue:hello-world.vue';
+	const [path, name] = arg.split(':');
+	return { path, name };
 };
 
 const { path: componentPath, name: componentName } = parseComponentArg();
 
 export default defineConfig({
-  plugins: [vue({
-    isProduction: true,
-  })],
-  build: {
-    outDir: './public',
-    emptyOutDir: false,
-
-    lib: {
-      entry: componentPath,
-      name: componentName,
-      formats: ['umd']
-    },
-    rollupOptions: {
-      external: ["vue", "lodash", "moment"],
-      output: {
-        dir: componentPath.replace(/\/[^\/]+$/, ""),
-        entryFileNames: `${componentName}.js`,
-        globals: {
-          vue: "Vue"
-        },
-      },
-    },
-  }
+	plugins: [
+		vue({
+			isProduction: true,
+		}),
+	],
+	build: {
+		emptyOutDir: false,
+		outDir: '../../public',
+		lib: {
+			entry: componentPath,
+			name: componentName,
+			formats: ['umd'],
+		},
+		rollupOptions: {
+			external: ['vue', 'lodash', 'moment'],
+			output: {
+				dir: './public',
+				entryFileNames: `${componentName}.js`,
+				globals: {
+					vue: 'Vue',
+				},
+			},
+		},
+	},
 });
