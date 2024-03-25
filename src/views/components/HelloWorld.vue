518bac17eb774e6b74d2a56009657b9f200a3fb9
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-24 20:17:01 -0500

    chore: wip

--- src/views/components/HelloWorld.vue
+++ src/views/components/HelloWorld.vue
@@ -6,7 +6,8 @@ const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ count }}</h1>
-
-  <button @click="count++">click</button>
+  <div>
+    <h1>{{ count }}</h1>
+    <button @click="count++">click</button>
+  </div>
 </template>
