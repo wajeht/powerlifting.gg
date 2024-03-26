ff695186be219e7b98b7369ccb65df2576084394
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-25 19:22:04 -0500

    fix: Update axios method in Users.vue and Users.test.js

--- src/views/components/Users/Users.test.js
+++ src/views/components/Users/Users.test.js
@@ -1,6 +1,24 @@
-// import Users from './Users.vue'
-import { it, expect } from 'vitest';
+import axios from 'axios';
+import Users from './Users.vue';
+import { it, vi } from 'vitest';
+import {render } from '@testing-library/vue';
 
-it('should work', async ()=> {
-  expect(true).toBeTruthy();
+vi.mock('axios');
+
+it.skip('should work', async ()=> {
+  axios.get.mockResolvedValue({ data: [{
+    "id": 14,
+    "username": "Kylee.Moen-Treutel23",
+    "email": "Rosetta.Bauch@hotmail.com",
+    "password": "$2a$10$gc6r7krvlLBEakYQYz5cZupxF5tuO3uGqmj/cJly4gzGmeiNEco8O",
+    "emoji": "🥉",
+    "role": "USER",
+    "tenant_id": 1,
+    "created_at": "2024-03-25 23:59:36",
+    "updated_at": "2024-03-25 23:59:36"
+  }]})
+
+  const { getByText } = render(Users);
+
+   getByText('x');
 });
--- src/views/components/Users/Users.vue
+++ src/views/components/Users/Users.vue
@@ -10,16 +10,15 @@ const computedDomain = computed(() => {
 });
 
 onMounted(async () => {
-  const { data } = await axios('/api/users');
+  const { data } = await axios.get('/api/users');
   states.users = data.data
-
 });
 </script>
 
 <template>
   <div class="flex flex-col gap-5">
-    <a v-for="user of states.users" :href="`${computedDomain}/user/${user.username}`" :key="`user-key-${user.id}`"
-      class="bg-neutral-200 hover:bg-neutral-300 p-5 rounded-md">
+    <a v-for="user of states.users" :href="`${computedDomain}/user/${user.username}`"
+      :key="`user-key-${user.id}`" class="bg-neutral-200 hover:bg-neutral-300 p-5 rounded-md">
       <h4 class="font-bold">{{ user.emoji }}</h4>
       <h4 class="font-bold">{{ user.username }}</h4>
       <p>{{ user.email }}</p>
