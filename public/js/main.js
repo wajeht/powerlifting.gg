58eb039a8c6030f5650b17ce0ec1b5c1f09ea1de
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-26 19:18:37 -0500

    refactor: update file names and axios configuration

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,15 @@ import './axios.min.js'; // 1.6.8
 import './Users.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.axios = axios;
+
+  window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
   const { createApp } = Vue;
+
   const app = createApp();
+
   app.component("Users", Users);
+
   app.mount("#app");
 });
