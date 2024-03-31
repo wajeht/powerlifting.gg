1704c4cc6918dafbaf8f50d18f8977b14c8e36f6
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-30 22:31:50 -0500

    feat: add tailwind typography css

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,14 @@
 import './axios.min.js'; // 1.6.8
 import './Users.js';
 
-  window.axios = axios;
+window.axios = axios;
 
-  window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-  const { createApp } = Vue;
+const { createApp } = Vue;
 
-  const app = createApp();
+const app = createApp();
 
-  app.component("Users", Users);
+app.component("Users", Users);
 
-  app.mount("#app");
+app.mount("#app");
