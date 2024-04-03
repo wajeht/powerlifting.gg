5310390b32a6807fe79a85b704cb15c660878454
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-02 21:16:58 -0500

    refactor: Update Search component with reactive and axios

--- public/js/Search.js
+++ public/js/Search.js
@@ -1,20 +1,68 @@
-import { onMounted as t, openBlock as o, createElementBlock as c, createElementVNode as e } from "vue";
-import { _ as s } from "./_plugin-vue_export-helper.js";
-const r = { class: "flex gap-1 w-full items-center justify-center" }, n = /* @__PURE__ */ e("input", {
-  type: "text",
-  placeholder: "Search for a coach or a system...",
-  class: "input input-bordered w-full max-w-[18rem]"
-}, null, -1), a = /* @__PURE__ */ e("button", { class: "btn btn-neutral" }, "Search", -1), l = [
-  n,
-  a
-], _ = {
+import { reactive as p, openBlock as o, createElementBlock as s, withDirectives as u, createElementVNode as r, vModelText as m, createCommentVNode as c, Fragment as h, renderList as _, normalizeClass as f, toDisplayString as l } from "vue";
+import g from "axios";
+import { _ as x } from "./_plugin-vue_export-helper.js";
+const b = { class: "flex gap-1 items-center justify-center relative w-[374px]" }, y = ["disabled"], w = ["disabled"], k = { key: 0 }, v = { key: 1 }, S = {
+  key: 0,
+  class: "menu absolute top-[55px] w-[374px] bg-neutral-100 rounded-sm"
+}, $ = ["href"], C = {
   __name: "Search",
-  setup(p) {
-    return t(() => {
-      console.log("live js");
-    }), (u, i) => (o(), c("div", r, [...l]));
+  setup(D) {
+    const e = p({
+      loading: !1,
+      search: "",
+      data: []
+    });
+    function i(a) {
+      const { protocol: n, hostname: t } = window.location;
+      return `${n}//${a}.${t}`;
+    }
+    async function d() {
+      e.loading = !0;
+      try {
+        const a = await g.get(`/api/search?q=${encodeURIComponent(e.search)}`);
+        e.data = a.data.data;
+      } catch (a) {
+        console.error("Error fetching data:", a);
+      } finally {
+        e.loading = !1;
+      }
+    }
+    return (a, n) => (o(), s("div", b, [
+      u(r("input", {
+        type: "text",
+        "onUpdate:modelValue": n[0] || (n[0] = (t) => e.search = t),
+        placeholder: "Search for a coach or a system...",
+        class: "input input-bordered w-full max-w-[18rem]",
+        disabled: e.loading
+      }, null, 8, y), [
+        [m, e.search]
+      ]),
+      r("button", {
+        onClick: d,
+        class: "btn btn-neutral",
+        disabled: e.loading
+      }, [
+        e.loading ? c("", !0) : (o(), s("span", k, "Search")),
+        e.loading ? (o(), s("span", v, "...........")) : c("", !0)
+      ], 8, w),
+      e.data.length ? (o(), s("ul", S, [
+        (o(!0), s(h, null, _(e.data, (t) => (o(), s("li", {
+          class: "flex flex-col gap-2",
+          key: t.id
+        }, [
+          r("a", {
+            href: i(t.slug)
+          }, [
+            r("div", {
+              class: f(["p-3", `bg-[${t.color}]`])
+            }, l(t.emoji), 3),
+            r("p", null, l(t.name), 1)
+          ], 8, $)
+        ]))), 128))
+      ])) : c("", !0)
+    ]));
   }
-}, d = /* @__PURE__ */ s(_, [["__file", "/usr/src/app/src/web/components/Search/Search.vue"]]);
+}, B = /* @__PURE__ */ x(C, [["__file", "/usr/src/app/src/web/components/Search/Search.vue"]]);
 export {
-  d as default
+  B as default
 };
--- src/web/components/Search/Search.vue
+++ src/web/components/Search/Search.vue
@@ -1,18 +1,55 @@
 <script setup>
-import { onMounted } from 'vue';
+import axios from 'axios';
+import { reactive } from 'vue';
 
-onMounted(() => {
-	console.log('live js');
+const states = reactive({
+	loading: false,
+	search: '',
+	data: [],
 });
+
+function computedDomain(slug) {
+	const { protocol, hostname } = window.location;
+	return `${protocol}//${slug}.${hostname}`;
+}
+
+async function fetchData() {
+	states.loading = true;
+	try {
+		const response = await axios.get(`/api/search?q=${encodeURIComponent(states.search)}`);
+		states.data = response.data.data;
+	} catch (error) {
+		console.error('Error fetching data:', error);
+	} finally {
+		states.loading = false;
+	}
+}
 </script>
 
 <template>
-	<div class="flex gap-1 w-full items-center justify-center">
+	<div class="flex gap-1 items-center justify-center relative w-[374px]">
 		<input
 			type="text"
+			v-model="states.search"
 			placeholder="Search for a coach or a system..."
 			class="input input-bordered w-full max-w-[18rem]"
+			:disabled="states.loading"
 		/>
-		<button class="btn btn-neutral">Search</button>
+		<button @click="fetchData" class="btn btn-neutral" :disabled="states.loading">
+			<span v-if="!states.loading">Search</span>
+			<span v-if="states.loading">...........</span>
+		</button>
+
+		<ul
+			v-if="states.data.length"
+			class="menu absolute top-[55px] w-[374px] bg-neutral-100 rounded-sm"
+		>
+			<li class="flex flex-col gap-2" v-for="tenant in states.data" :key="tenant.id">
+				<a :href="computedDomain(tenant.slug)">
+					<div class="p-3" :class="`bg-[${tenant.color}]`">{{ tenant.emoji }}</div>
+					<p>{{ tenant.name }}</p>
+				</a>
+			</li>
+		</ul>
 	</div>
 </template>
