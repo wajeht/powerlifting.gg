9a5cee451070e7eb700ab538a783739dc99568e7
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-07 16:22:03 -0500

    refactor: Update number of seeded tenants to 100

--- src/database/seeds/01_tenant_seeder.js
+++ src/database/seeds/01_tenant_seeder.js
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker';
 
 export async function seed(db) {
-	const tenants = Array.from({ length: 10 }, () => ({
+	const tenants = Array.from({ length: 100 }, () => ({
 		name: faker.company.name(),
 		slug: faker.lorem.slug(),
 		emoji: faker.internet.emoji(),
