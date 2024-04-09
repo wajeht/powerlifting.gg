01e4adf100e8458a40415d3798b1a149a778e534
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-09 18:50:18 -0500

    style: Remove unnecessary space in script-src

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ if (appConfig.env === 'production') {
 			contentSecurityPolicy: {
 				directives: {
 					...helmet.contentSecurityPolicy.getDefaultDirectives(),
-					'default-src': ["'self'", 'plausible.jaw.dev '],
+					'default-src': ["'self'", 'plausible.jaw.dev'],
 					'script-src': [
 						"'self'",
 						"'unsafe-inline'",
@@ -52,6 +52,8 @@ if (appConfig.env === 'production') {
 						'localhost',
 						'plausible.jaw.dev',
 					],
+					'script-src-attr': ["'none'"],
+					'script-src-elem': ["'self'", 'powerlifting.gg', 'localhost', 'plausible.jaw.dev'],
 				},
 			},
 		}),
