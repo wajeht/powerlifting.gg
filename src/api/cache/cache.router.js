009bb0bdb31434f29d620b2dc91409c227a295fa
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-09 23:51:17 -0500

    chore: Comment out unnecessary code block in cache router

--- src/api/cache/cache.router.js
+++ src/api/cache/cache.router.js
@@ -11,22 +11,23 @@ cache.get(
 	catchAsyncErrorHandler(async (req, res) => {
 		const keys = await redis.keys('*');
 
-		if (req.query.data === 'true') {
-			const data = {};
+		// // TODO: this might not be a good idea
+		// if (req.query.data === 'true') {
+		// 	const data = {};
 
-			for (const key of keys) {
-				const value = await redis.get(key);
+		// 	for (const key of keys) {
+		// 		const value = await redis.get(key);
 
-				if (!key.includes(appConfig.session.store_prefix)) {
-					data[key] = value;
-				}
-			}
+		// 		if (!key.includes(appConfig.session.store_prefix)) {
+		// 			data[key] = value;
+		// 		}
+		// 	}
 
-			return res.status(200).json({
-				message: 'ok',
-				data,
-			});
-		}
+		// 	return res.status(200).json({
+		// 		message: 'ok',
+		// 		data,
+		// 	});
+		// }
 
 		return res.status(200).json({
 			message: 'ok',
