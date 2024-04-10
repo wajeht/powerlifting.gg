e309183c6559ac71512a2eb4461870ec96bc7b3d
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-10 11:34:49 -0500

    feat: Add new tenantIdentityHandler middleware

--- src/api/cache/cache.router.js
+++ src/api/cache/cache.router.js
@@ -1,13 +1,16 @@
 import express from 'express';
 
-import { catchAsyncErrorHandler } from '../../app.middlewares.js';
+import { catchAsyncErrorHandler, tenantIdentityHandler } from '../../app.middlewares.js';
 import { redis } from '../../database/db.js';
 import { app as appConfig } from '../../config/app.js';
+import { NotFoundError } from '../../app.errors.js';
 
 const cache = express.Router();
 
 cache.get(
 	'/',
+	tenantIdentityHandler,
+	(req, res, next) => (req.tenant ? next(new NotFoundError()) : next()),
 	catchAsyncErrorHandler(async (req, res) => {
 		const keys = await redis.keys('*');
 
