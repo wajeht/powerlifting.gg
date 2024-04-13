3631da06e578b7ef88cf61f9ccd76c837baf5a5b
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-13 14:17:07 -0500

    refactor: Update security settings for production environment

--- src/app.js
+++ src/app.js
@@ -51,43 +51,45 @@ app.use(flash());
 app.use(compression());
 app.disable('x-powered-by');
 
-app.use(
-	cors({
-		credentials: true,
-		origin: true,
-	}),
-);
-app.use(
-	helmet({
-		contentSecurityPolicy: {
-			directives: {
-				...helmet.contentSecurityPolicy.getDefaultDirectives(),
-				'default-src': ["'self'", 'plausible.jaw.dev', 'powerlifting.gg', 'app.test'],
-				'script-src': [
-					"'self'",
-					'app.test',
-					"'unsafe-inline'",
-					'plausible.jaw.dev',
-					"'unsafe-eval'",
-					'powerlifting.gg',
-					'localhost',
-					'blob:',
-					'text/javascript',
-				],
+if (appConfig.env === 'production') {
+	app.use(
+		cors({
+			credentials: true,
+			origin: true,
+		}),
+	);
+	app.use(
+		helmet({
+			contentSecurityPolicy: {
+				directives: {
+					...helmet.contentSecurityPolicy.getDefaultDirectives(),
+					'default-src': ["'self'", 'plausible.jaw.dev', 'powerlifting.gg', 'app.test'],
+					'script-src': [
+						"'self'",
+						'app.test',
+						"'unsafe-inline'",
+						'plausible.jaw.dev',
+						"'unsafe-eval'",
+						'powerlifting.gg',
+						'localhost',
+						'blob:',
+						'text/javascript',
+					],
+				},
 			},
-		},
-	}),
-);
-app.use(
-	rateLimit({
-		windowMs: 15 * 60 * 1000, // 15 minutes
-		limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-		standardHeaders: 'draft-7',
-		legacyHeaders: false,
-		handler: rateLimitHandler,
-		skip: skipOnMyIp,
-	}),
-);
+		}),
+	);
+	app.use(
+		rateLimit({
+			windowMs: 15 * 60 * 1000, // 15 minutes
+			limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+			standardHeaders: 'draft-7',
+			legacyHeaders: false,
+			handler: rateLimitHandler,
+			skip: skipOnMyIp,
+		}),
+	);
+}
 
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true }));
