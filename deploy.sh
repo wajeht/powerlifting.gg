4fa288cc8168f7bfa4fa45db8c86c653467b96da
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-13 19:27:01 -0500

    fix: Update deployment script for resetting to origin branch

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,9 @@ ssh root@5.78.109.1 << EOF
 
 cd powerlifting.gg
 
-# docker compose -f docker-compose.prod.yml down --rmi all
+git fetch origin
 
-# docker stop $(docker ps -aq) && docker system prune -af --volumes
-
-# docker system prune -a --volumes -f
-
-git reset HEAD .
-
-git checkout -- .
-
-git pull
-
-git checkout feature/cookie
+git reset --hard origin/feature/cookie
 
 docker compose -f docker-compose.prod.yml up -d --build
 
