1ab91635aa9201d40d5311259edce63f3909d149
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-18 18:34:58 -0500

    fix: Fix typo in docker compose command

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ git fetch origin main
 
 git reset --hard origin/main
 
-docker-compose -f docker-compose.prod.yml build --no-cache powerlifting
+docker compose -f docker-compose.prod.yml build --no-cache powerlifting
 
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml up -d
 
 EOF
