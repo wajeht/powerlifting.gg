b0966440ca951e4e55495741f469fa291363ede5
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-25 18:55:39 -0500

    test: add test for healthz and contact page handlers

--- src/web/web.test.js
+++ src/web/web.test.js
@@ -10,37 +10,39 @@ const app = request(server);
 
 await refreshDatabase();
 
-it('should be able to get /healthz end point with json', async () => {
-	const res = await app.get('/healthz').set('Content-Type', 'application/json');
-	expect(res.status).toBe(200);
-	expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
-	expect(res.body).toHaveProperty('message');
-	expect(res.body).toHaveProperty('uptime');
-});
-
-it('should be able to get /healthz end point with html', async () => {
-	const res = await app.get('/healthz');
-	expect(res.headers['content-type']).toBe('text/html; charset=utf-8');
-	expect(res.status).toBe(200);
-	expect(res.text).include('ok');
-});
-
-describe('when visiting / route, if there is no tenant', () => {
-	it('should go to the main domain page', async () => {
-		const tenants = Array.from({ length: 5 }, () => ({
-			name: faker.company.name(),
-			slug: faker.lorem.slug(),
-		}));
-
-		await db('tenants').insert(tenants);
-
-		const res = await app.get('/');
+describe('getHealthzHandler', () => {
+	it('should be able to get /healthz end point with json', async () => {
+		const res = await app.get('/healthz').set('Content-Type', 'application/json');
 		expect(res.status).toBe(200);
-		expect(res.text).include("Let's Find Your");
+		expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
+		expect(res.body).toHaveProperty('message');
+		expect(res.body).toHaveProperty('uptime');
+	});
+
+	it('should be able to get /healthz end point with html', async () => {
+		const res = await app.get('/healthz');
+		expect(res.headers['content-type']).toBe('text/html; charset=utf-8');
+		expect(res.status).toBe(200);
+		expect(res.text).include('ok');
 	});
 });
 
-describe('index', () => {
+describe('getIndexHandler', () => {
+	describe('when visiting / route, if there is no tenant', () => {
+		it('should go to the main domain page', async () => {
+			const tenants = Array.from({ length: 5 }, () => ({
+				name: faker.company.name(),
+				slug: faker.lorem.slug(),
+			}));
+
+			await db('tenants').insert(tenants);
+
+			const res = await app.get('/');
+			expect(res.status).toBe(200);
+			expect(res.text).include("Let's Find Your");
+		});
+	});
+
 	it('should be able to visit a tenant page', async () => {
 		const tenant = await db('tenants')
 			.insert({
@@ -53,5 +55,27 @@ describe('index', () => {
 
 		expect(res.statusCode).toBe(200);
 		expect(res.text).contain('obama');
+	});
+});
+
+describe('getContactHandler', () => {
+	it('should not be able to get contact page with tenant domain', async () => {
+		const tenant = await db('tenants')
+			.insert({
+				name: 'thanks',
+				slug: 'obama',
+			})
+			.returning('*');
+		const res = await app
+			.get('/contact')
+			.set('Host', `${tenant[0].slug}.${appEnv.development_app_url}`);
+		expect(res.statusCode).toBe(404);
+		expect(res.text).contain('Oops! The page you are looking for cannot be found.');
+	});
+
+	it('should be able to get contact page', async () => {
+		const res = await app.get('/contact');
+		expect(res.statusCode).toBe(200);
+		expect(res.text).contain('Use the contact form to get in touch or email us');
 	});
 });
