3f8e4f59a8342fbb30fc168a12de680fda5bb8c2
wajeht <58354193+wajeht@users.noreply.github.com>
2024-04-25 19:44:54 -0500

    test: Add tests for getting tenants by ID with subdomain

--- src/api/tenant/tenant.test.js
+++ src/api/tenant/tenant.test.js
@@ -1,24 +1,38 @@
-import { db } from '../../database/db.js';
 import request from 'supertest';
+import { db } from '../../database/db.js';
 import { it, expect, describe } from 'vitest';
-
 import { app as server } from '../../app.js';
 import { refreshDatabase } from '../../tests/refresh-db.js';
+import { app as appConfig } from '../../config/app.js';
 
 const app = request(server);
 
 await refreshDatabase();
 
 describe('getAllTenantHandler', () => {
+	it('should be able to get /api/tenants/:id end point with subdomain', async () => {
+		const [tenant] = await db('tenants')
+			.insert({
+				name: 'thanks',
+				slug: 'obama',
+			})
+			.returning('*');
+		const res = await app
+			.get(`/api/tenants/${tenant.id}`)
+			.set('Host', `${tenant.slug}.${appConfig.development_app_url}`);
+		expect(res.statusCode).toBe(404);
+		expect(res.text).contain('Oops! The page you are looking for cannot be found.');
+	});
+
 	it('should be able to get /api/tenants/:id end point', async () => {
-		const tenant = await db('tenants')
+		const [tenant] = await db('tenants')
 			.insert({
 				name: 'thanks',
 				slug: 'obama',
 			})
 			.returning('*');
 
-		const res = await app.get(`/api/tenants/${tenant[0].id}`);
+		const res = await app.get(`/api/tenants/${tenant.id}`);
 
 		expect(res.statusCode).toBe(200);
 		expect(res.body.data[0]).toStrictEqual(tenant.data);
@@ -26,6 +40,20 @@ describe('getAllTenantHandler', () => {
 });
 
 describe('getTenantHandler', () => {
+	it('should be able to get /api/tenants end point with subdomain', async () => {
+		const [tenant] = await db('tenants')
+			.insert({
+				name: 'thanks',
+				slug: 'obama',
+			})
+			.returning('*');
+		const res = await app
+			.get('/api/tenants')
+			.set('Host', `${tenant.slug}.${appConfig.development_app_url}`);
+		expect(res.statusCode).toBe(404);
+		expect(res.text).contain('Oops! The page you are looking for cannot be found.');
+	});
+
 	it('should be able to get /api/tenants end point', async () => {
 		const tenants = await db('tenants')
 			.insert([
