1e50f7e06b765643da094ed70cb638f515d10ef2
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-03 23:11:42 -0500

    feat: Add SUDO_PASSWORD variable to .env and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 source .env
 
+SUDO_PASSWORD=$(grep -i "^SUDO_PASSWORD=" .env | cut -d '=' -f 2)
+
 ssh -T $PRODUCTION_SSH_URL << EOF
 
 if [ ! -d "powerlifting.gg" ]; then
@@ -14,6 +16,6 @@ git fetch origin main
 
 git reset --hard origin/main
 
-docker compose -f docker-compose.prod.yml up -d --build
+echo $SUDO_PASSWORD | sudo -S docker compose -f docker-compose.prod.yml up -d --build
 
 EOF
