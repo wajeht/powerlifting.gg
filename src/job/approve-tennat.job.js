e6f2901c92a20b884316d0fbb099870c13a1a06f
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-04 16:46:05 -0500

    feat: Add approve tenant email job functionality

---
+++ src/job/approve-tennat.job.js
@@ -0,0 +1,28 @@
+import { Queue, Worker } from 'bullmq';
+import { logger } from '../utils/logger.js';
+import { redis } from '../database/db.js';
+import { sendApproveTenantEmail } from '../emails/email.js';
+
+const queueName = 'sendApproveTenantEmailQueue';
+
+export const sendApproveTenantEmailQueue = new Queue(queueName, {
+	connection: redis,
+});
+
+const processSendApproveTenantEmailJob = async (job) => {
+	try {
+		await sendApproveTenantEmail({
+			email: job.data.email,
+			username: job.data.username,
+		});
+		logger.info(`Approve Tenant email job  sent to ${job.data.email}`);
+	} catch (error) {
+		logger.error(`Failed to send Approve Tenant email job to ${job.data.email}`, error);
+	}
+};
+
+new Worker(queueName, processSendApproveTenantEmailJob, { connection: redis });
+
+export async function sendWelcomeEmailJob(data) {
+	await sendApproveTenantEmailQueue.add('sendApproveTenantEmailJob', data);
+}
