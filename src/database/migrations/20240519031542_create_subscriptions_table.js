a5652d050f3bf4588204c5ed519a47429c9a9ad8
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-18 22:30:21 -0500

    refactor: Change column name 'subscriptions' to 'type'

--- src/database/migrations/20240519031542_create_subscriptions_table.js
+++ src/database/migrations/20240519031542_create_subscriptions_table.js
@@ -7,13 +7,13 @@ export function up(knex) {
 		table.increments('id').index();
 		table.string('email').notNullable().unique().index(); // no `user_id` so public user can subscript to it
 		table
-			.text('subscriptions')
+			.text('type')
 			.notNullable()
 			.defaultTo(
 				JSON.stringify({
 					newsletter: false,
 					changelog: false,
-					promotions: false,
+					promotion: false,
 				}),
 			);
 		table.timestamps(true, true);
