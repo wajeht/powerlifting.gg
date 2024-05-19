d60378f14af8b725e2e13f4397dd83292f6feecf
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-19 13:07:11 -0500

    feat: Add feature to send newsletter emails if today is newsletter day

--- src/job/newsletter.js
+++ src/job/newsletter.js
@@ -1,8 +1,25 @@
-import { db } from '../database/db.js';
+import dayjs from 'dayjs';
+import { job } from './job.js';
+import { db, redis } from '../database/db.js';
+import { WebService } from '../web/web.service.js';
+import { WebRepository } from '../web/web.repository.js';
 
-let users = await db
-	.select('*')
-	.from('subscriptions')
-	.where('preferences', 'like', '%"newsletter":true%');
+let todayPost = await WebService(WebRepository(db), redis, job).getBlogPosts({ cache: false });
+todayPost = dayjs(todayPost[0].meta.date).format('YYYY-MM-DD');
 
-console.log(users);
+const today = dayjs().format('YYYY-MM-DD');
+
+if (today === todayPost) {
+	let users = await db
+		.select('*')
+		.from('subscriptions')
+		.where('type', 'like', '%"newsletter":true%');
+
+	for (const email of users) {
+		await job.sendNewsletterEmailJob({
+			email,
+			username: 'xxx',
+			post: 'xxx',
+		});
+	}
+}
