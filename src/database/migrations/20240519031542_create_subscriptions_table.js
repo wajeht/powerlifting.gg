8492a689c7d4ee238c6c922b0b39be02dd256c81
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-18 22:17:43 -0500

    feat: Add migration for creating subscriptions table

---
+++ src/database/migrations/20240519031542_create_subscriptions_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+	return knex.schema.createTable('subscriptions', function (table) {
+		table.increments('id').index();
+		table.string('email').notNullable().unique().index();
+		table.boolean('newsletter').defaultTo(false);
+		table.boolean('changelog').defaultTo(false);
+		table.boolean('promotions').defaultTo(false);
+		table.timestamps(true, true);
+	});
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+	return knex.schema.dropTable('subscriptions');
+}
