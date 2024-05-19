adff6152191a95043e146e3f0464f54569bedfa3
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-19 11:49:58 -0500

    feat: Add newsletter job to send emails

---
+++ src/job/newsletter.job.js
@@ -0,0 +1,27 @@
+import { Queue, Worker } from 'bullmq';
+import { logger } from '../utils/logger.js';
+import { redis } from '../database/db.js';
+import { sendNewsletterEmail } from '../emails/email.js';
+
+const queueName = 'sendNewsletterEmailQueue';
+
+export const sendNewsletterEmailQueue = new Queue(queueName, {
+	connection: redis,
+});
+
+const processSendNewsletterEmailJob = async (job) => {
+	try {
+		job.updateProgress(0);
+		await sendNewsletterEmail({ email: job.data.email, username: job.data.username });
+		job.updateProgress(100);
+		logger.info(`Newsletter email job  sent to ${job.data.email}`);
+	} catch (error) {
+		logger.error(`Failed to send newsletter email job to ${job.data.email}`, error);
+	}
+};
+
+new Worker(queueName, processSendNewsletterEmailJob, { connection: redis });
+
+export async function sendNewsletterEmailJob(data) {
+	await sendNewsletterEmailQueue.add('sendNewsletterEmailJob', data);
+}
