3ee72e1f22cec4898ce65bddc2eb5e815e94733a
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-18 22:29:17 -0500

    refactor: Update subscriptions table schema and test script

--- src/database/migrations/20240519031542_create_subscriptions_table.js
+++ src/database/migrations/20240519031542_create_subscriptions_table.js
@@ -5,10 +5,17 @@
 export function up(knex) {
 	return knex.schema.createTable('subscriptions', function (table) {
 		table.increments('id').index();
-		table.string('email').notNullable().unique().index();
-		table.boolean('newsletter').defaultTo(false);
-		table.boolean('changelog').defaultTo(false);
-		table.boolean('promotion').defaultTo(false);
+		table.string('email').notNullable().unique().index(); // no `user_id` so public user can subscript to it
+		table
+			.text('subscriptions')
+			.notNullable()
+			.defaultTo(
+				JSON.stringify({
+					newsletter: false,
+					changelog: false,
+					promotions: false,
+				}),
+			);
 		table.timestamps(true, true);
 	});
 }
