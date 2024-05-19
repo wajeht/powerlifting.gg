1be459eb0d92bd8e60498811e47e67e07e4ff4f8
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-19 11:55:29 -0500

    feat: Add sendNewsletterEmail function

--- src/emails/email.js
+++ src/emails/email.js
@@ -1,3 +1,4 @@
 export { sendWelcomeEmail } from './templates/welcome/welcome.js';
 export { sendContactEmail } from './templates/contact/contact.js';
+export { sendNewsletterEmail } from './templates/newsletter/newsletter.js';
 export { sendApproveTenantEmail } from './templates/approve-tenant/approve-tenant.js';
