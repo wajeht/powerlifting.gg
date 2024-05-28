317d26d920205c57f9afb02b086e2a765d03854c
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-28 16:59:27 -0500

    refactor: Improve indentation and code formatting

--- src/utils/discord.js
+++ src/utils/discord.js
@@ -7,38 +7,38 @@ import axios from 'axios';
 /* It sends a message to a Discord channel */
 // https://gist.github.com/Birdie0/78ee79402a4301b1faf412ab5f1cdcf9
 export class Discord {
-  static async send(msg, object = null) {
-    try {
-      let params = null;
+	static async send(msg, object = null) {
+		try {
+			let params = null;
 
-      // use different format to send if we have object passed in
-      if (object == null) {
-        params = { username: 'Powerlifting.gg', content: msg };
-      } else {
-        params = {
-          username: 'Powerlifting.gg',
-          content: msg,
-          embeds: [
-            {
-              title: msg,
-              description: object,
-            },
-          ],
-        };
-      }
+			// use different format to send if we have object passed in
+			if (object == null) {
+				params = { username: 'Powerlifting.gg', content: msg };
+			} else {
+				params = {
+					username: 'Powerlifting.gg',
+					content: msg,
+					embeds: [
+						{
+							title: msg,
+							description: object,
+						},
+					],
+				};
+			}
 
-      let res = null;
+			let res = null;
 
-      // only send chad message to discord in production environment
-      // prettier-ignore
-      if (appConfig.env === 'production') {
+			// only send chad message to discord in production environment
+			// prettier-ignore
+			if (appConfig.env === 'production') {
         res = await axios({ method: 'POST', headers: { 'Content-Type': 'application/json', }, data: JSON.stringify(params), url: discordConfig.url });
         if (res?.status === 204) logger.info(`Chad sent ${msg}`);
       } else {
         logger.warn(`Skipping Chad message in dev environment!`)
       }
-    } catch (e) {
-      logger.error(e);
-    }
-  }
+		} catch (e) {
+			logger.error(e);
+		}
+	}
 }
