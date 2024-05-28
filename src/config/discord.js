71e44f7524710cb796eb08d864b3aac3d1db00db
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-27 22:19:37 -0500

    refactor: Update import paths in discord.js and Discord class

--- src/config/discord.js
+++ src/config/discord.js
@@ -1,7 +1,7 @@
 import './env.js';
 
 export const discord = Object.freeze({
-  id: process.env.DISCORD_ID,
-  token: process.env.DISCORD_TOKEN,
-  url: process.env.DISCORD_URL,
+	id: process.env.DISCORD_ID,
+	token: process.env.DISCORD_TOKEN,
+	url: process.env.DISCORD_URL,
 });
