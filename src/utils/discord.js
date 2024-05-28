c7e521a3ea1d0e2088e645554ac3cce8e73654c1
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-28 11:16:39 -0500

    feat: Update Discord message sending function

--- src/utils/discord.js
+++ src/utils/discord.js
@@ -1,44 +1,37 @@
 import { logger } from '../utils/logger.js';
 import { app as appConfig } from '../config/app.js';
 import { discord as discordConfig } from '../config/discord.js';
-
 import axios from 'axios';
 
 /* It sends a message to a Discord channel */
 // https://gist.github.com/Birdie0/78ee79402a4301b1faf412ab5f1cdcf9
-export class Discord {
-  static async send(msg, object = null) {
-    try {
-      let params = null;
-
-      // use different format to send if we have object passed in
-      if (object == null) {
-        params = { username: 'Powerlifting.gg', content: msg };
-      } else {
-        params = {
-          username: 'Powerlifting.gg',
-          content: msg,
-          embeds: [
-            {
-              title: msg,
-              description: object,
-            },
-          ],
-        };
-      }
-
-      let res = null;
-
-      // only send chad message to discord in production environment
-      // prettier-ignore
-      if (appConfig.env === 'production') {
-        res = await axios({ method: 'POST', headers: { 'Content-Type': 'application/json', }, data: JSON.stringify(params), url: discordConfig.url });
-        if (res?.status === 204) logger.info(`Chad sent ${msg}`);
-      } else {
-        logger.warn(`Skipping Chad message in dev environment!`)
-      }
-    } catch (e) {
-      logger.error(e);
+export async function alertDiscord(msg, object = null) {
+  try {
+    if (appConfig.env !== 'production') {
+      logger.warn('Skipping Discord bot in dev environment!');
+      return;
     }
+
+    let params;
+
+    if (object === null) {
+      params = { username: 'Powerlifting.gg', content: msg };
+    } else {
+      params = {
+        username: 'Powerlifting.gg',
+        content: msg,
+        embeds: [{ title: msg, description: object }]
+      };
+    }
+
+    const res = await axios.post(discordConfig.url, params, {
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (res.status === 204) {
+      logger.info(`Discord bot has sent: ${msg}`);
+    }
+  } catch (error) {
+    logger.error(error);
   }
 }
