8747a0e661c6c46b5276e46f6b62d5a0a63b5d54
wajeht <58354193+wajeht@users.noreply.github.com>
2024-05-31 17:57:44 -0500

    feat: Add new admin handlers for approvals and cache management

--- src/web/admin/admin.router.js
+++ src/web/admin/admin.router.js
@@ -1,9 +1,4 @@
-import path from 'path';
-import fs from 'fs/promises';
 import express from 'express';
-import { db, redis } from '../../database/db.js';
-import { session as sessionConfig } from '../../config/session.js';
-import { NotFoundError } from '../../app.error.js';
 import {
 	authenticationHandler,
 	authorizePermissionHandler,
@@ -13,7 +8,15 @@ import {
 	csrfHandler,
 } from '../../app.middleware.js';
 
-import { getAdminHandler } from './admin.handler.js';
+import {
+	deleteCacheHandler,
+	getAdminHandler,
+	getCacheHandler,
+	getDatabaseHandler,
+	getReviewsHandler,
+	getTenantsHandler,
+	postApproveTenantHandler,
+} from './admin.handler.js';
 
 const admin = express.Router();
 
@@ -33,23 +36,7 @@ admin.post(
 	authenticationHandler,
 	authorizePermissionHandler('SUPER_ADMIN'),
 	csrfHandler,
-	catchAsyncErrorHandler(async (req, res) => {
-		const ids = req.body.id.map((id) => parseInt(id));
-
-		const tenants = await db
-			.select('*')
-			.from('tenants')
-			.whereIn('id', ids)
-			.andWhere({ approved: false });
-
-		if (!tenants.length) throw new NotFoundError();
-
-		await db('tenants').whereIn('id', ids).update({ approved: true });
-
-		tenants.forEach((tenant) => req.flash('success', `${tenant.slug} has been approved!`));
-
-		return res.redirect('/admin/tenants');
-	}),
+	catchAsyncErrorHandler(postApproveTenantHandler()),
 );
 
 admin.get(
@@ -59,27 +46,7 @@ admin.get(
 	authenticationHandler,
 	authorizePermissionHandler('SUPER_ADMIN'),
 	csrfHandler,
-	catchAsyncErrorHandler(async (req, res) => {
-		const reviews = await db('reviews')
-			.select(
-				'reviews.*',
-				'users.username',
-				'users.profile_picture',
-				'tenants.slug',
-				'tenants.name as tenant_name',
-			)
-			.join('users', 'reviews.user_id', 'users.id')
-			.join('tenants', 'reviews.tenant_id', 'tenants.id')
-			.orderBy('reviews.created_at', 'desc');
-
-		return res.status(200).render('./admin/reviews.html', {
-			reviews,
-			flashMessages: req.flash(),
-			title: 'Admin / Reviews',
-			path: '/admin/reviews',
-			layout: '../layouts/admin.html',
-		});
-	}),
+	catchAsyncErrorHandler(getReviewsHandler()),
 );
 
 admin.get(
@@ -89,17 +56,7 @@ admin.get(
 	authenticationHandler,
 	authorizePermissionHandler('SUPER_ADMIN'),
 	csrfHandler,
-	catchAsyncErrorHandler(async (req, res) => {
-		const tenants = await db.select('*').from('tenants').orderBy('created_at', 'desc');
-
-		return res.status(200).render('./admin/tenants.html', {
-			tenants,
-			flashMessages: req.flash(),
-			title: 'Admin / Tenants',
-			path: '/admin/tenants',
-			layout: '../layouts/admin.html',
-		});
-	}),
+	catchAsyncErrorHandler(getTenantsHandler()),
 );
 
 admin.get(
@@ -109,31 +66,7 @@ admin.get(
 	authenticationHandler,
 	authorizePermissionHandler('SUPER_ADMIN'),
 	csrfHandler,
-	catchAsyncErrorHandler(async (req, res) => {
-		const backupFiles = await fs.readdir(path.resolve(process.cwd(), 'src', 'database', 'backup'));
-
-		let backup = [];
-		for (const file of backupFiles) {
-			if (file.endsWith('.sqlite.gz')) {
-				let dateStr = file.replace('db-', '').replace('.sqlite.gz', '');
-				backup.push({
-					path: path.resolve(process.cwd(), 'src', 'database', 'backup', file),
-					created_at: dateStr,
-					name: file,
-				});
-			}
-		}
-
-		backup.sort((a, b) => b.created_at.localeCompare(a.created_at));
-
-		return res.status(200).render('./admin/database.html', {
-			backup,
-			flashMessages: req.flash(),
-			title: 'Admin / Database',
-			path: '/admin/database',
-			layout: '../layouts/admin.html',
-		});
-	}),
+	catchAsyncErrorHandler(getDatabaseHandler()),
 );
 
 admin.get(
@@ -143,49 +76,7 @@ admin.get(
 	authenticationHandler,
 	authorizePermissionHandler('SUPER_ADMIN'),
 	csrfHandler,
-	catchAsyncErrorHandler(async (req, res) => {
-		const cache = [];
-		const keys = await redis.keys('*');
-
-		for (const key of keys) {
-			const type = await redis.type(key);
-			let value;
-
-			switch (type) {
-				case 'string':
-					value = await redis.get(key);
-					break;
-				case 'hash':
-					value = await redis.hgetall(key);
-					break;
-				case 'list':
-					value = await redis.lrange(key, 0, -1);
-					break;
-				case 'set':
-					value = await redis.smembers(key);
-					break;
-				case 'zset':
-					value = await redis.zrange(key, 0, -1, 'WITHSCORES');
-					break;
-				default:
-					value = 'Unknown type';
-					break;
-			}
-
-			if (!key.startsWith('bull:') && !key.startsWith(sessionConfig.store_prefix)) {
-				const ttl = await redis.ttl(key);
-				cache.push({ key, ttl, value });
-			}
-		}
-
-		return res.status(200).render('./admin/cache.html', {
-			cache,
-			flashMessages: req.flash(),
-			title: 'Admin / Cache',
-			path: '/admin/cache',
-			layout: '../layouts/admin.html',
-		});
-	}),
+	catchAsyncErrorHandler(getCacheHandler()),
 );
 
 admin.post(
@@ -195,23 +86,7 @@ admin.post(
 	authenticationHandler,
 	authorizePermissionHandler('SUPER_ADMIN'),
 	csrfHandler,
-	catchAsyncErrorHandler(async (req, res) => {
-		if (req.body.method === 'delete') {
-			const key = req.body.key;
-
-			const found = await redis.get(key);
-
-			if (!found) throw new NotFoundError();
-
-			await redis.del(key);
-
-			req.flash('success', `Cache ${key} has been deleted!`);
-
-			return res.redirect('/admin/cache');
-		}
-
-		throw new NotFoundError();
-	}),
+	catchAsyncErrorHandler(deleteCacheHandler()),
 );
 
 export { admin };
