76df5736ee3d7f92a241aaac114fbe8a94e3d50c
wajeht <58354193+wajeht@users.noreply.github.com>
2024-06-03 21:34:20 -0500

    refactor: rename validation function for postCalibrateTenantReviews

--- src/web/web.validation.js
+++ src/web/web.validation.js
@@ -2,7 +2,7 @@ import { body, param, query } from 'express-validator';
 import { db } from '../database/db.js';
 import { ValidationError } from '../app.error.js';
 
-export const postCalibrateTenantReviews = [
+export const postCalibrateTenantReviewsValidation = [
 	param('id')
 		.notEmpty()
 		.withMessage('The id must not be empty!')
