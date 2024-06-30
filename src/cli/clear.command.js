e9fd7e9f3d07f74573dd71f08d1f66ea878f2c08
wajeht <58354193+wajeht@users.noreply.github.com>
2024-06-30 14:58:18 -0500

    style: Fix missing semicolons and indentation in CLI files

--- src/cli/clear.command.js
+++ src/cli/clear.command.js
@@ -1,23 +1,21 @@
-
-import { cloudflare as cloudflareConfig } from '../config/cloudflare.js'
+import { cloudflare as cloudflareConfig } from '../config/cloudflare.js';
 import { clearCloudflareCache } from './utils/clear-cloudflare-cache.js';
-import { logger } from '../utils/logger.js'
+import { logger } from '../utils/logger.js';
 
 export async function clear(option) {
-  const { cloudflare_cache } = option;
+	const { cloudflare_cache } = option;
 
-  if (cloudflare_cache) {
-    try {
+	if (cloudflare_cache) {
+		try {
+			await clearCloudflareCache({
+				zoneId: cloudflareConfig.POWERLIFTINGDOTGG_ZONE_ID,
+				apiToken: cloudflareConfig.PURGE_CACHE_API_KEY,
+			});
 
-      await clearCloudflareCache({
-        zoneId: cloudflareConfig.POWERLIFTINGDOTGG_ZONE_ID,
-        apiToken: cloudflareConfig.PURGE_CACHE_API_KEY
-      });
-
-      logger.info('Cache cleared');
-    } catch (error) {
-      const message = error.response.data.errors.map(e => e.message).join(' ')
-      logger.error(message);
-    }
-  }
+			logger.info('Cache cleared');
+		} catch (error) {
+			const message = error.response.data.errors.map((e) => e.message).join(' ');
+			logger.error(message);
+		}
+	}
 }
