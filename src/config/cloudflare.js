1f5a23eca7ff2a37dc34abc52a9fca4fe3377257
wajeht <58354193+wajeht@users.noreply.github.com>
2024-06-30 15:15:47 -0500

    refactor: Update variable names in clear command for clarity

--- src/config/cloudflare.js
+++ src/config/cloudflare.js
@@ -1,7 +1,7 @@
 import './env.js';
 
 export const cloudflare = Object.freeze({
-	PURGE_CACHE_API_KEY: process.env.CLOUDFLARE_PURGE_CACHE_API_KEY,
-	POWERLIFTINGDOTGG_ZONE_ID: process.env.CLOUDFLARE_POWERLIFTINGDOTGG_ZONE_ID,
-	EMAIL: process.env.CLOUDFLARE_EMAIL,
+	api_key: process.env.CLOUDFLARE_PURGE_CACHE_API_KEY,
+	zone_id: process.env.CLOUDFLARE_POWERLIFTINGDOTGG_ZONE_ID,
+	email: process.env.CLOUDFLARE_EMAIL,
 });
