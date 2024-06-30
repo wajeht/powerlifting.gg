847e654d6e977e53aaa801764c896c2624aaed67
wajeht <58354193+wajeht@users.noreply.github.com>
2024-06-30 14:57:52 -0500

    feat: clear cli command

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander';
+import { clear } from './clear.command.js'
 
 const program = new Command();
 
@@ -7,6 +9,12 @@ program
 	.name('powerlifting.gg')
 	.description('multitenancy coaching review systems')
 	.version('0.0.1');
+
+program
+	.command('clear')
+	.description('clear some stuff')
+	.option('-cc, --cloudflare_cache', 'cloudflare cache')
+	.action(async (option) => await clear(option));
 
 if (process.argv.length < 3) {
 	console.log('POWERLIFTING.GG');
