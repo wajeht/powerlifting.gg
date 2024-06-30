242599e467b9b1c9e0b176457719fea6a56f5e49
wajeht <58354193+wajeht@users.noreply.github.com>
2024-06-30 15:23:08 -0500

    style: update comment to plural form

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ const server = app.listen(appConfig.port, async () => {
 		}
 	}
 
-	// command
+	// commands
 	if (appConfig.env === 'production') {
 		await shell('powerlifting clear --cloudflare_cache');
 	}
