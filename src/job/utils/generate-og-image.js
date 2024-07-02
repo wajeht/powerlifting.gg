bf2c923a3e67cb131efaaec9ca08337dbe28a445
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-02 10:47:33 -0500

    feat: Add progress updates and update og_image URL

--- src/job/utils/generate-og-image.js
+++ src/job/utils/generate-og-image.js
@@ -4,9 +4,12 @@ import sharp from 'sharp';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { publicS3BucketConfig, backBlaze } from '../../config/back-blaze.js';
 import { logger } from '../../utils/logger.js';
+import { db } from '../../database/db.js';
 
 export async function generateOgImage({ tenant, job }) {
 	try {
+		job.updateProgress(0);
+
 		const bannerUrl = tenant.banner;
 		const response = await axios({
 			url: bannerUrl,
@@ -15,7 +18,11 @@ export async function generateOgImage({ tenant, job }) {
 
 		const imageKey = `${Date.now().toString()}.jpeg`;
 
+		job.updateProgress(25);
+
 		const resizedImageBuffer = await sharp(response.data).resize(1200, 630).jpeg().toBuffer();
+
+		job.updateProgress(75);
 
 		const uploadParams = {
 			Bucket: process.env.PUBLIC_BACKBLAZE_BUCKET,
@@ -27,12 +34,15 @@ export async function generateOgImage({ tenant, job }) {
 		const command = new PutObjectCommand(uploadParams);
 		await publicS3BucketConfig.send(command);
 
+		logger.info(`og image uploaded successfully for tenant ${tenant.slug}`);
+
+		job.updateProgress(100);
+
 		const uploadedImageUrl = `${backBlaze.public.end_point}/${process.env.PUBLIC_BACKBLAZE_BUCKET}/${imageKey}`;
 
-		logger.info(`Image uploaded successfully for job ${job.id}`);
-		return uploadedImageUrl;
+		await db('tenants').where({ id: tenant.id }).update({ og_image: uploadedImageUrl });
 	} catch (error) {
-		logger.error('Error processing image:', error);
+		logger.error('error processing og image:', error);
 		throw error;
 	}
 }
