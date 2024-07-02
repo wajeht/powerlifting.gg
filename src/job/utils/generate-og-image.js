867c156bec147faf4c6fc288a0fd618ab957d0c5
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-02 10:40:58 -0500

    feat: Add function to generate og image with AWS S3 integration

--- src/job/utils/generate-og-image.js
+++ src/job/utils/generate-og-image.js
@@ -1,3 +1,38 @@
+import axios from 'axios';
+import sharp from 'sharp';
+
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { publicS3BucketConfig, backBlaze } from '../../config/back-blaze.js';
+import { logger } from '../../utils/logger.js';
+
 export async function generateOgImage({ tenant, job }) {
-	console.log(job, tenant);
+	try {
+		const bannerUrl = tenant.banner;
+		const response = await axios({
+			url: bannerUrl,
+			responseType: 'arraybuffer',
+		});
+
+		const imageKey = `${Date.now().toString()}.jpeg`;
+
+		const resizedImageBuffer = await sharp(response.data).resize(1200, 630).jpeg().toBuffer();
+
+		const uploadParams = {
+			Bucket: process.env.PUBLIC_BACKBLAZE_BUCKET,
+			Key: imageKey,
+			Body: resizedImageBuffer,
+			ContentType: 'image/jpeg',
+		};
+
+		const command = new PutObjectCommand(uploadParams);
+		await publicS3BucketConfig.send(command);
+
+		const uploadedImageUrl = `${backBlaze.public.end_point}/${process.env.PUBLIC_BACKBLAZE_BUCKET}/${imageKey}`;
+
+		logger.info(`Image uploaded successfully for job ${job.id}`);
+		return uploadedImageUrl;
+	} catch (error) {
+		logger.error('Error processing image:', error);
+		throw error;
+	}
 }
