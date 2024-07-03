a15e34466589d86fd6472b5201a527ff09533883
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-03 18:17:52 -0500

    feat: Add directory creation for SSL certificates

--- scripts/letsencrypt.sh
+++ scripts/letsencrypt.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+source .env
+
+SUDO_PASSWORD=$(grep -i "^SUDO_PASSWORD=" .env | cut -d '=' -f 2)
+
 # Ensure the directories for the certificates exist
-mkdir -p ./letsencrypt
+mkdir -p ./letsencrypt ./www
 
 # Run the Certbot container to generate SSL certificates
-docker run --rm \
+echo $SUDO_PASSWORD | sudo -S docker run --name certbot_temp --rm \
   -v "$(pwd)/letsencrypt:/etc/letsencrypt" \
   -v "$(pwd)/www:/var/www/certbot" \
   certbot/certbot certonly --standalone \
@@ -13,6 +17,10 @@ docker run --rm \
   --email noreply@powerlifting.gg \
   --agree-tos \
   --no-eff-email
+
+# Ensure the Certbot container is stopped and removed
+echo $SUDO_PASSWORD | sudo -S docker stop certbot_temp || true
+echo $SUDO_PASSWORD | sudo -S docker rm certbot_temp || true
 
 # Notify the user of the script completion
 echo "SSL certificates generated and stored in ./letsencrypt directory."
