0844b6ad8db7718a400ccadb05ba9de02d28939b
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-03 11:25:50 -0500

    feat: Add reviews count to getAllMyTenants query

--- src/web/web.repository.js
+++ src/web/web.repository.js
@@ -17,10 +17,12 @@ export function WebRepository(db) {
 		},
 		getAllMyTenants: async (userId) => {
 			return await db
-				.select('*')
+				.select('tenants.*')
 				.from('tenants')
+				.leftJoin('reviews', 'tenants.id', 'reviews.tenant_id')
 				.join('coaches', 'coaches.tenant_id', 'tenants.id')
 				.where({ 'coaches.user_id': userId })
+				.count('reviews.id as reviews_count')
 				.orderBy('tenants.created_at', 'desc');
 		},
 		getApprovedTenants: async () => {
