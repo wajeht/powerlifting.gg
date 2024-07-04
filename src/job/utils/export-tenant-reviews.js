a3a35c37e564974e0d26b83020cff8f3ab6de56b
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-04 00:49:11 -0500

    feat: Add papaparse for exporting CSV reviews

--- src/job/utils/export-tenant-reviews.js
+++ src/job/utils/export-tenant-reviews.js
@@ -1,3 +1,26 @@
-export function exportTenantReviews({ job, tenant_id }) {
-	console.log(job, tenant_id);
+import papa from 'papaparse';
+import { db } from '../../database/db.js';
+import { logger } from '../../utils/logger.js';
+import fs from 'fs';
+
+export async function exportTenantReviews({ job, tenant_id }) {
+	try {
+		job.updateProgress(0);
+
+		const reviews = await db.select('*').from('reviews').where({ tenant_id });
+
+		const filePath = `./${Date.now().toString()}-tenant-id-${tenant_id}-reviews.csv`;
+		const writableStream = fs.createWriteStream(filePath);
+
+		const csv = papa.unparse(reviews, { header: true });
+
+		writableStream.write(csv);
+
+		writableStream.end(() => {
+			logger.info('File successfully written!');
+			job.updateProgress(100);
+		});
+	} catch (error) {
+		logger.error('failed to export tenant reviews', error);
+	}
 }
