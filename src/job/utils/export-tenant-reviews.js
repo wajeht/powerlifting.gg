a83aead92dd86da3fb2ebaedd286538f5793b1a0
wajeht <58354193+wajeht@users.noreply.github.com>
2024-07-04 01:05:20 -0500

    feat: Add functionality to export tenant reviews to S3

--- src/job/utils/export-tenant-reviews.js
+++ src/job/utils/export-tenant-reviews.js
@@ -1,26 +1,57 @@
+import fs from 'fs';
 import papa from 'papaparse';
+
 import { db } from '../../database/db.js';
 import { logger } from '../../utils/logger.js';
-import fs from 'fs';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { publicS3BucketConfig, backBlaze } from '../../config/back-blaze.js';
 
 export async function exportTenantReviews({ job, tenant_id }) {
 	try {
 		job.updateProgress(0);
 
 		const reviews = await db.select('*').from('reviews').where({ tenant_id });
-
-		const filePath = `./${Date.now().toString()}-tenant-id-${tenant_id}-reviews.csv`;
-		const writableStream = fs.createWriteStream(filePath);
+		job.updateProgress(25);
 
 		const csv = papa.unparse(reviews, { header: true });
+		job.updateProgress(50);
 
-		writableStream.write(csv);
+		const filePath = `./src/tmp/${Date.now().toString()}-tenant-id-${tenant_id}-reviews.csv`;
+		fs.writeFileSync(filePath, csv);
+		job.updateProgress(75);
 
-		writableStream.end(() => {
-			logger.info('File successfully written!');
-			job.updateProgress(100);
-		});
+		const fileStream = fs.createReadStream(filePath);
+		const fileName = filePath.split('/').pop();
+		const uploadParams = {
+			Bucket: process.env.PUBLIC_BACKBLAZE_BUCKET,
+			Key: fileName,
+			Body: fileStream,
+			ContentType: 'text/csv',
+		};
+
+		const command = new PutObjectCommand(uploadParams);
+		await publicS3BucketConfig.send(command);
+
+		const uploadedImageUrl = `${backBlaze.public.end_point}/${process.env.PUBLIC_BACKBLAZE_BUCKET}/${fileName}`;
+		logger.info(`CSV file successfully uploaded to S3: ${uploadedImageUrl}`);
+		job.updateProgress(100);
+
+		fs.unlinkSync(filePath);
 	} catch (error) {
+		logger.error(error);
 		logger.error('failed to export tenant reviews', error);
+		job.updateProgress(100);
 	}
 }
+
+// function j() {
+//     return {
+//         updateProgress: function (value) {
+//             console.log(value);
+//         }
+//     }
+// }
+
+// const job = j();
+
+// await exportTenantReviews({ job, tenant_id: 1 })
